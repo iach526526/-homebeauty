6bc363ce8c0888924ba5e464bea526a74c66f1a7
iach526526 <iach526526@gmail.com>
2024-01-19 13:24:16 +0800

    first commit

---
+++ app/src/main/java/nutc/edu/homebeauty/MainActivity.java
@@ -0,0 +1,59 @@
+package nutc.edu.homebeauty;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.KeyEvent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myweb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myweb = findViewById(R.id.myWeb);
+        myweb.getSettings().setJavaScriptEnabled(true);
+
+        // 設置自定義的 WebViewClient
+        myweb.setWebViewClient(new MyWebViewClient());
+
+        myweb.loadUrl("https://homebeauty.com.tw");
+    }
+
+    // 自定義 WebViewClient
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            Uri url = request.getUrl();
+            String host = url.getHost();
+            return false;
+            // 如果連結指向指定的網站，就在 WebView 中打開
+//            if ("homebeauty.com.tw".equals(host)) {
+//                return false;
+//            }
+//
+//            // 否則，使用外部瀏覽器打開
+//            Intent intent = new Intent(Intent.ACTION_VIEW, url);
+//            startActivity(intent);
+//            return true;
+        }
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check whether the key event is the Back button and if there's history.
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myweb.canGoBack()) {
+            myweb.goBack();
+            return true;
+        }
+        // If it isn't the Back button or there's no web page history, bubble up to
+        // the default system behavior. Probably exit the activity.
+        return super.onKeyDown(keyCode, event);
+    }
+}
